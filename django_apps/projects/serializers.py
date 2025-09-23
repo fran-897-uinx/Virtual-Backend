331c0c8969380008ac06ac304f3558b3dee93b2b
fran-897-uinx <prevailfrancis@gmail.com>
2025-09-23 13:36:56 +0100

    sloved the project view  for the frontend

--- django_apps/projects/serializers.py
+++ django_apps/projects/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Project
 
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = "__all__"
+
+    def get_image(self, obj):
+        request = self.context.get("request")
+        if obj.image:
+            return request.build_absolute_uri(obj.image.url)
+        return None
